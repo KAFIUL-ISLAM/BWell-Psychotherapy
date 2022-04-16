554a135dd749680c0c9120db8489a517f7896342
Al Kafi <alkafi28@gmail.com>
2022-04-16 21:49:33 +0600

    checking url

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import Banner from './Components/Homepage/Banner/Banner';
 
 function App() {
   return (
     <div >
       <Routes>
-        <Route path='/' element={}></Route>
+        <Route path='/' element={<Banner></Banner>}></Route>
+        <Route path='*' element={<h1 className='fs-8'>404 Not Found</h1>}></Route>
       </Routes>
     </div>
   );
