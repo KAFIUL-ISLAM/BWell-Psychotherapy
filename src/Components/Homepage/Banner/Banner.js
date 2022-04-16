ab8bba437b571ecd2cf0252bfe79cffec64f05c0
Al Kafi <alkafi28@gmail.com>
2022-04-16 21:47:38 +0600

    initial commit

---
+++ src/Components/Homepage/Banner/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            <h1 className='fs-12'>Project Building....</h1>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/firebase.init.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/app/dist/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC95ECiaWwHTLEHeITR7G9UYiyJmf-vSW8",
+    authDomain: "bwell-psychotherapy.firebaseapp.com",
+    projectId: "bwell-psychotherapy",
+    storageBucket: "bwell-psychotherapy.appspot.com",
+    messagingSenderId: "1015427412757",
+    appId: "1:1015427412757:web:850714a6114653e624ef1c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export default auth;
